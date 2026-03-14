ebf597d3b8a26c3ad0d882a1fd01922a21296a1d
niloccolinus <niloccolin@protonmail.com>
2026-03-14 17:23:44 +0100

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
@@ -11,6 +12,7 @@ import remarkCode from "./src/remark/remarkCode.ts";
 export default defineConfig({
     site: "https://coleslow.dev",
     integrations: [
+        sitemap(),
         mdx({
             extendMarkdownConfig: false,
             remarkPlugins: [remarkMath, remarkShader, remarkCode],
